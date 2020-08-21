7a7f88dfef60fe27800bd2ea8100fbd05d83553d
Particle45 <69759289+Particle45@users.noreply.github.com>
2020-08-21 20:52:08 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -36,4 +36,14 @@ self.addEventListener('push', function(event) {
     event.waitUntil(self.registration.showNotification(title, options));
   });
 
+    self.addEventListener('notificationclick', function(event) {
+    console.log('[Service Worker] Notification click received.');
   
+    event.notification.close();
+  
+    event.waitUntil(
+      clients.openWindow('https://developers.google.com/web')
+    );
+  });
+
+
